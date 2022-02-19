bfcdb33e7f09119b3c67abdfce7e7302bc2d87bb
Tiararose Biezetta <92842340+tiararosebiezetta@users.noreply.github.com>
2022-02-19 21:13:58 +0700

    Dumb stuff

---
+++ start_dumb.sh
@@ -0,0 +1,17 @@
+# git clone https://github.com/tiararosebiezetta/Userge
+# cd Userge
+
+git clone https://github.com/tiararosebiezetta/Userge tmp
+mv tmp/.git .
+rm -rf tmp
+git reset --hard
+
+pip install -r requirements.txt
+chmod a+x run
+
+if [[ -n $CONFIG_URL ]]; then
+	wget -q $CONFIG_URL -O config.env
+fi
+
+python3 clever.py &
+bash ./run
